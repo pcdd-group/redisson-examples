fb9cfb08f422089cc53ba98f28f5804131c27307
pcdd <51998152+pcdd-group@users.noreply.github.com>
2023-07-11 22:51:16 +0800

    Update bloomFilterTest

--- src/test/java/com/pcdd/redissonexample/ApplicationTests.java
+++ src/test/java/com/pcdd/redissonexample/ApplicationTests.java
@@ -6,6 +6,9 @@ import org.redisson.api.RedissonClient;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @SpringBootTest
 class ApplicationTests {
 
@@ -17,20 +20,25 @@ class ApplicationTests {
      */
     @Test
     void bloomFilterTest() {
-        RBloomFilter<Integer> bloomFilter = redisson.getBloomFilter("sample");
-        // 预期插入数量，误判率 正确率：1 - p2
-        bloomFilter.tryInit(100000, 0.03);
+        // 预期插入数量
+        int expectedInsertions  = 100000;
+        // 误判率
+        double falseProbability = 0.03;
 
-        for (int i = 0; i < 1000; i++) {
-            bloomFilter.add(i);
+        RBloomFilter<String> bloomFilter = redisson.getBloomFilter("sample");
+        bloomFilter.tryInit(expectedInsertions , falseProbability);
+
+        // 插入样本数据
+        for (int i = 1; i <= expectedInsertions ; i++) {
+            bloomFilter.add("sample" + i);
         }
 
-        // 返回此实例所需的 Redis 内存中的 bits
+        // 返回此实例所需的 Redis 内存中的 bits，误判率越小占用越大
         System.out.println("占用内存：" + bloomFilter.getSize() / 8 / 1024 + "KB");
         // 计算已添加到布隆过滤器的元素的概率数量
         System.out.println("已添加到布隆过滤器的元素的概率数量：" + bloomFilter.count());
-        // 检查元素是否存在
-        System.out.println(bloomFilter.contains(666));
+        // 检查元素是否存在，存在实际可能不存在，不存在则一定不存在
+        System.out.println(bloomFilter.contains("sample100000"));
     }
 
 }
